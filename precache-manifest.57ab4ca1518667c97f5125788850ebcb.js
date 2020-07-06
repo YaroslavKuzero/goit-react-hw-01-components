200ef5c69bbe2356c6a1ac8e6602a7ead990345c
YaroslavKuzero <49403383+YaroslavKuzero@users.noreply.github.com>
2020-07-07 00:35:05 +0300

    Updates

---
+++ precache-manifest.57ab4ca1518667c97f5125788850ebcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9846fc89b8e56298555b51d1f22664ab",
+    "url": "/github.io/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "df0a79e608fa9e56fe4a",
+    "url": "/github.io/goit-react-hw-01-components/static/css/main.f0f45020.chunk.css"
+  },
+  {
+    "revision": "13a030a57f3648a9d35e",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.e5c56e05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.e5c56e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0a79e608fa9e56fe4a",
+    "url": "/github.io/goit-react-hw-01-components/static/js/main.1e79a971.chunk.js"
+  },
+  {
+    "revision": "d543f3f7f76a2ad070f8",
+    "url": "/github.io/goit-react-hw-01-components/static/js/runtime-main.57b42ba3.js"
+  }
+]);
